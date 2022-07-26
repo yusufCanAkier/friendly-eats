197165d85a96b26d6661020a7a25a15409f2264e
YUSUF CAN AKIER <83148529+yusufCanAkier@users.noreply.github.com>
2022-07-26 22:25:43 +0300

    some changes

--- scripts/FriendlyEats.Data.js
+++ scripts/FriendlyEats.Data.js
@@ -47,29 +47,29 @@ FriendlyEats.prototype.getRestaurant = function(id) {
   return firebase.firestore().collection('restaurants').doc(id).get();
 };
 
-FriendlyEats.prototype.getFilteredRestaurants = function(filters, renderer) {
-  var query = firebase.firestore().collection('restaurants');
+// FriendlyEats.prototype.getFilteredRestaurants = function(filters, renderer) {
+//   var query = firebase.firestore().collection('restaurants');
 
-  if (filters.category !== 'Any') {
-    query = query.where('category', '==', filters.category);
-  }
+//   if (filters.category !== 'Any') {
+//     query = query.where('category', '==', filters.category);
+//   }
 
-  if (filters.city !== 'Any') {
-    query = query.where('city', '==', filters.city);
-  }
+//   if (filters.city !== 'Any') {
+//     query = query.where('city', '==', filters.city);
+//   }
 
-  if (filters.price !== 'Any') {
-    query = query.where('price', '==', filters.price.length);
-  }
+//   if (filters.price !== 'Any') {
+//     query = query.where('price', '==', filters.price.length);
+//   }
 
-  if (filters.sort === 'Rating') {
-    query = query.orderBy('avgRating', 'desc');
-  } else if (filters.sort === 'Reviews') {
-    query = query.orderBy('numRatings', 'desc');
-  }
+//   if (filters.sort === 'Rating') {
+//     query = query.orderBy('avgRating', 'desc');
+//   } else if (filters.sort === 'Reviews') {
+//     query = query.orderBy('numRatings', 'desc');
+//   }
 
-  this.getDocumentsInQuery(query, renderer);
-};
+//   this.getDocumentsInQuery(query, renderer);
+// };
 
 FriendlyEats.prototype.addRating = function(restaurantID, rating) {
   var collection = firebase.firestore().collection('restaurants');
